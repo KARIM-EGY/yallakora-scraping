e6ffe7d3d2f031aef9b99d964ee0db23564596f0
Karim Mohammed <karim228223@gmail.com>
2024-06-25 23:35:38 +0300

    Update scraping.py

--- scraping.py
+++ scraping.py
@@ -3,8 +3,7 @@ from bs4 import BeautifulSoup
 import requests
 
 date = input('write the date to get the matches of the date (MM/DD/YYYY) : ')
-URL = (f'https://www.yallakora.com/match-center/%D9%85%D8%B1%D9%83%D8%B2-%D8%A7%D9%84%D9%85%D8%A8%D8%A7%D8%B1%D9%8A'
-       f'%D8%A7%D8%AA?date={date}')
+URL = (f'https://www.yallakora.com/match-center/%D9%85%D8%B1%D9%83%D8%B2-%D8%A7%D9%84%D9%85%D8%A8%D8%A7%D8%B1%D9%8A%D8%A7%D8%AA?date={date}')
 # requesting the data from the website
 page = requests.get(URL).content
 match_table = []  # creating list of the output
